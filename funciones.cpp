32d7f128eeabe2b7bc4cfd3e8e6bcd2f5b19fd9f
Ingenieria Sistemas31 <isi31@lab.uca.edu.ni>
2023-04-21 08:25:39 -0600

    metPro

--- funciones.cpp
+++ funciones.cpp
@@ -1,21 +1,28 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
-#define MAX 10
-//arreglo.
+#define MAX 100
 
+//Arreglo.
 int edades[MAX];
-int pos=0;//posicon del elemento
+int pos=0; //posicion del elemento
 
 void agregarEdad(int edad);
-bool hayEspacio();
+bool hayEspacios();
+void mostrarEdades();
+
 
 void agregarEdad(int edad){
-edades[pos] = edad;
-
-pos++;
+    edades[pos] = edad;
+    pos++;
 }
 
 bool hayEspacios(){
-    return pos< MAX;
+    return pos < MAX;
+}
+
+void mostrarEdades(){
+    for(int i = 0; i < pos; i++){
+        cout << edades [i] << endl;
+    }
 }
